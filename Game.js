12eaf8ecfd2e377690537a497610c16c7199a24a
Mohammad Anees <aman6129@neo.tamu.edu>
2014-03-28 13:52:03 -0500

    Big Update
    Includes cannons, scoreboards, game ending screen.
    Need to add networking functionality

--- Game.js
+++ Game.js
@@ -10,6 +10,8 @@ window.addEventListener("load",function() {
 	var _vx = 0;
 	var _vy = 0;
 
+	var _angle = 45;
+
 	var _score = window.innerWidth/2;
 
 	function randomPos() //returns a random position inside the window
@@ -44,17 +46,27 @@ window.addEventListener("load",function() {
 		_vx = (_x/60);
 		_vy = (_y/60);
 
+		//Calc Cannon angle here
+
+		_angle = Math.atan(_y / _x);
+		_angle *= (180/Math.PI)
+
+		//
+
 		return true;
 	}
 
 	function DrawSomething(mp)
 	{
-		Q.stage(0).insert(new Q.Food());
 
-		var obj = Q.stage().locate(0,0);
+		if(_x <= 400 && _y <= 400)
+		{
+			Q.stage(1).insert(new Q.Food());
 
-		if(_x <= 300 && _y <= 300)
-			obj.animate({ x : _x * 100, y: _y * 100, angle: 360 * 10}, 40, Q.Easing.Quadratic.Out);
+			var obj = Q.stage(1).locate(0,0);
+
+			obj.animate({ x : _x * 100, y: _y * 100, angle: 360 * 10}, 60, Q.Easing.Quadratic.Out);
+		}
 
 	}
 
@@ -105,7 +117,7 @@ window.addEventListener("load",function() {
 		this.on("hit.sprite", function(collision) {
 
 			if(collision.obj.isA("Floor")) {
-				this.p.vy = -(1/2);
+				//this.destroy();
 			}
 
 			else if(collision.obj.isA("Fatty")) {
@@ -124,6 +136,22 @@ window.addEventListener("load",function() {
 
 	})
 
+	Q.Sprite.extend("Cannon", {
+		init: function(p) {
+			this._super({
+				asset: "cannon.png",
+				x: 0,
+				y: 0,
+				angle: 45
+			})
+		},
+
+		step: function(dt) {
+			//document.onmousemove = getMousePosition;
+			this.p.angle = _angle;
+		}
+	})
+
 	Q.Sprite.extend("Floor", {
 		init: function(p) {
 			this._super({
@@ -140,15 +168,15 @@ window.addEventListener("load",function() {
 			this._super({
 				asset: "stick.png",
 				x: window.innerWidth/2,
-				y: window.innerHeight - 10
+				y: window.innerHeight - 50
 			})
 		},
 
 		step: function(dt){
 			this.p.x = _score;
 
-			if(_score >= (window.innerWidth/2 + 150))
-				Q.clearStages();
+			if(_score >= (window.innerWidth/2 + 500))
+				Q.stageScene("endGame",1, { label: "You Won!" });
 
 
 		}
@@ -156,23 +184,99 @@ window.addEventListener("load",function() {
 
 	})
 
-
-	Q.scene("level1",function(stage) {
-
-		var fatass = stage.insert(new Q.Fatty());
-		var orange = stage.insert(new Q.Food());
-		var orange2 = stage.insert(new Q.Food());
-		var floor = stage.insert(new Q.Floor());
-		var score = stage.insert(new Q.ScoreBoard());
-
-		orange.p.y = _y;
-		orange.p.x = _x;
+	Q.scene('endGame',function(stage) {
+	  var box = stage.insert(new Q.UI.Container({
+	    x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
+	  }));
+	  
+	  var button = box.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC",
+	                                           label: "GGWP" }))         
+	  var label = box.insert(new Q.UI.Text({x:10, y: -10 - button.p.h, 
+	                                        label: stage.options.label }));
+	  button.on("click",function() {
+	    Q.clearStages();
+	    Q.stageScene('level1');
+	  });
+	  box.fit(20);
 	});
 
 
+	Q.Sprite.extend("View", {
+		init:function(p) {
+			this._super({
+				asset: "view.png",
+				x: window.innerWidth/2,
+				y: window.innerHeight/2
+			})
+		},
+
+		step: function(dt){
+			this.p.x += .5;
+		}
+	})
+
+	Q.Sprite.extend("ProgBar", {
+		init:function(p) {
+			this._super({
+				asset: "meter_bar.png",
+				x: window.innerWidth/2,
+				y: window.innerHeight - 50
+
+			})
+		}
+	})
+
+
+
+	Q.scene("level1",function(stage) {
+
+		var reedtard = stage.insert(new Q.Fatty());
+		var orange2 = stage.insert(new Q.Food());
+	
+		var floor = stage.insert(new Q.Floor());
+		var prog_bar = stage.insert(new Q.ProgBar());
+		var score = stage.insert(new Q.ScoreBoard());
+
+
+		//orange.p.y = _y;
+		//orange.p.x = _x;
+	});
+
+	Q.scene("level2", function(stage) {
+
+
+	
+	stage.insert(new Q.Repeater({ asset: "sky.png",
+										repeatX: true,
+										repeatY: true,
+										speedX: 4,
+										type: 0
+										}));
+								
+	// stage.insert(new Q.Repeater({ asset: "bush.png",
+	// 									repeatX: true,
+	// 									repeatY: false,
+	// 									speedX:   13,
+	// 									y: 400
+	// 									}));
+
+
+	var view = stage.insert(new Q.View());
+	stage.add("viewport").follow(view);
+
+});
+
+	Q.scene("cannnon", function(stage) {
+
+		var el_canon = stage.insert(new Q.Cannon());
+	})
+
+
 	// Make sure fatty.png is loaded
-	Q.load("fatty.png, food_1.png, food_2.png, food_3.png, floor.png, sky.jpg, stick.png",function() {
-		Q.stageScene("level1", 0);
+	Q.load("fatty.png, food_1.png, food_2.png, food_3.png, floor.png, sky.png, stick.png, bush.png, view.png, meter_bar.png, cannon.png",function() {
+		Q.stageScene("cannnon",2);
+		Q.stageScene("level1", 1);
+		Q.stageScene("level2", 0);
 
 	});
 });
